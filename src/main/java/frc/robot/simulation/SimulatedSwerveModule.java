e3699ebdc05340802ff6bf59dae7f5a390f19c9a
Jonas Brastad <jonas.brastad@misd400.org>
2024-01-12 15:10:20 -0800

    Added simmed swerve mod class, not much in it yet

--- src/main/java/frc/robot/simulation/SimulatedSwerveModule.java
+++ src/main/java/frc/robot/simulation/SimulatedSwerveModule.java
@@ -3,13 +3,18 @@ package frc.robot.simulation;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import frc.robot.SwerveModule;
+import frc.robot.Constants.SwerveConstants;
 
 public class SimulatedSwerveModule extends SwerveModule {
-
+    FlywheelSim steerMotor = new FlywheelSim(DCMotor.getFalcon500(1), SwerveConstants.kSteerMotorGearRatio, 0.004096955);
+    FlywheelSim driveMotor = new FlywheelSim(DCMotor.getFalcon500(1), /* i have no idea but i think we should google it */);
     public SimulatedSwerveModule(int moduleNumber, Rotation2d angleOffset, SwerveModuleConstants moduleConstants) {
         super(moduleNumber, angleOffset, moduleConstants);
-        
+
     }
     
 }
