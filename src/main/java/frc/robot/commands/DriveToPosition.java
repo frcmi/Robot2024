4f7d67f098da0576311644c48f50bf911dfee057
Nora Beda <theo.github@bedafamily.com>
2024-02-14 15:42:07 -0800

    java semantics lol

--- src/main/java/frc/robot/commands/DriveToPosition.java
+++ src/main/java/frc/robot/commands/DriveToPosition.java
@@ -13,42 +13,44 @@ import frc.lib.math.Transformations;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.SwerveConstants;
 import frc.robot.subsystems.SwerveSubsystem;
+
 /**
  * Command that drives the robot to a field position
  */
 public class DriveToPosition extends Command {
-    SwerveSubsystem swerve;
-    Supplier<Pose2d> positionSupplier;
-    Pose2d destination;
-    PIDController xPID = new PIDController(
-        AutoConstants.kAutoXP,
-        AutoConstants.kAutoXI,
-        AutoConstants.kAutoXD);
-    PIDController yPID = new PIDController(
-        AutoConstants.kAutoYP,
-        AutoConstants.kAutoYI,
-        AutoConstants.kAutoYD);
-    PIDController turnPID = new PIDController(
-        AutoConstants.kRotationP,
-        AutoConstants.kRotationI,
-        AutoConstants.kRotationD);
+    private SwerveSubsystem swerve;
+    private Supplier<Pose2d> positionSupplier;
+    private Pose2d destination;
 
-    StructPublisher<Pose2d> positionPublisher = NetworkTableInstance.getDefault()
-        .getStructTopic("DriveToPosition", Pose2d.struct).publish();
+    private static PIDController xPID = new PIDController(
+            AutoConstants.kAutoXP,
+            AutoConstants.kAutoXI,
+            AutoConstants.kAutoXD);
+    private static PIDController yPID = new PIDController(
+            AutoConstants.kAutoYP,
+            AutoConstants.kAutoYI,
+            AutoConstants.kAutoYD);
+    private static PIDController turnPID = new PIDController(
+            AutoConstants.kRotationP,
+            AutoConstants.kRotationI,
+            AutoConstants.kRotationD);
+
+    private static StructPublisher<Pose2d> positionPublisher = NetworkTableInstance.getDefault()
+            .getStructTopic("DriveToPosition", Pose2d.struct).publish();
 
     /**
      * Drive robot to a given position using PID
+     * 
      * @param swerveSubsystem the swerve subsystem to spin motors on
-     * @param currentPosition a supplier that contains the consitently updated position of the bot
-     * @param destination the position that the robot should drive to, relative to the field
+     * @param currentPosition a supplier that contains the consitently updated
+     *                        position of the bot
+     * @param destination     the position that the robot should drive to, relative
+     *                        to the field
      */
     public DriveToPosition(SwerveSubsystem swerveSubsystem, Supplier<Pose2d> currentPosition, Pose2d dst) {
         addRequirements(swerveSubsystem);
-        // SmartDashboard.setDefaultNumber("move pid P", 0.2);
-        // SmartDashboard.setDefaultNumber("move pid I", 0);
-        // SmartDashboard.setDefaultNumber("move pid D", 0);
         swerve = swerveSubsystem;
-        
+
         positionSupplier = currentPosition;
         destination = dst;
 
@@ -56,7 +58,7 @@ public class DriveToPosition extends Command {
         yPID.setSetpoint(destination.getY());
         turnPID.setSetpoint(destination.getRotation().getRadians());
         positionPublisher.set(destination);
-        
+
         SmartDashboard.putData("Move PID", xPID);
         SmartDashboard.putData("Rotation PID", turnPID);
 
@@ -64,7 +66,7 @@ public class DriveToPosition extends Command {
     }
 
     StructPublisher<Pose2d> setPositionPublisher = NetworkTableInstance.getDefault()
-        .getStructTopic("set position", Pose2d.struct).publish();
+            .getStructTopic("set position", Pose2d.struct).publish();
 
     @Override
     public void execute() {
@@ -72,7 +74,7 @@ public class DriveToPosition extends Command {
         xPID = (PIDController) SmartDashboard.getData("Move PID");
         turnPID = (PIDController) SmartDashboard.getData("Rotation PID");
         yPID.setPID(xPID.getP(), xPID.getI(), xPID.getD());
-        
+
         var pose = positionSupplier.get();
         double x = xPID.calculate(pose.getX());
         double y = yPID.calculate(pose.getY());
@@ -85,8 +87,10 @@ public class DriveToPosition extends Command {
     @Override
     public boolean isFinished() {
         var toDestination = positionSupplier.get().minus(destination);
-        return Transformations.length(toDestination.getTranslation()) <= SwerveConstants.kAllowedDistanceToDestination &&
-               Math.abs(toDestination.getRotation().getRadians()) <= SwerveConstants.kAllowedRotationDifferenceToDestination;
+        return Transformations.length(toDestination.getTranslation()) <= SwerveConstants.kAllowedDistanceToDestination
+                &&
+                Math.abs(toDestination.getRotation()
+                        .getRadians()) <= SwerveConstants.kAllowedRotationDifferenceToDestination;
     }
-    
+
 }
