f71fa9df580f30b44b9355b390a271f380842f4f
Nora Beda <theo.github@bedafamily.com>
2024-03-01 17:37:41 -0800

    fix up auto align math

--- src/main/java/frc/robot/commands/AutoAlignCommand.java
+++ src/main/java/frc/robot/commands/AutoAlignCommand.java
@@ -7,7 +7,9 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.lib.math.Transformations;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class AutoAlignCommand {
@@ -17,36 +19,45 @@ public class AutoAlignCommand {
         swerve = swerveSubsystem;
     }
 
-    private static final Transform3d robotToShooter = new Transform3d(0, 0, 0, new Rotation3d(Math.PI / 6, 0, 0));
+    private static final Transform3d robotToShooter = new Transform3d(0, 0, 0, new Rotation3d(0, Math.PI / 6, Math.PI));
     private static final Pose3d speaker = new Pose3d(16.427, 5.548, 2.032, new Rotation3d(0, 0, Math.PI));
     private static final double maximumFiringAngle = 75 * Math.PI / 180;
 
     private Pose2d calculateDestination() {
-        Pose3d currentPose = new Pose3d(swerve.getPose());
-        Transform3d speakerToRobot = currentPose.minus(speaker);
+        Pose3d robot = new Pose3d(swerve.getPose());
+        Pose3d shooter = robot.plus(robotToShooter);
+        Transform3d speakerToRobot = robot.minus(speaker);
 
         // restrict the angle to a -180 to 180 degree range
-        double angle = speakerToRobot.getRotation().getZ();
-        while (Math.abs(angle) > Math.PI)
+        double targetAngle = speakerToRobot.getRotation().getZ();
+        while (Math.abs(targetAngle) > Math.PI)
         {
-            angle -= Math.PI * 2 * Math.signum(angle);
+            targetAngle -= Math.PI * 2 * Math.signum(targetAngle);
         }
 
         // make sure were moving to a valid firing position
-        angle = MathUtil.clamp(angle, -maximumFiringAngle, maximumFiringAngle);
+        targetAngle = MathUtil.clamp(targetAngle, -maximumFiringAngle, maximumFiringAngle);
+
+        // get pitch of shooter
+        var shooterDirection = new Translation3d(1, shooter.getRotation());
+        var shooterDirectionHorizontal = Transformations.normalize(shooterDirection.toTranslation2d());
+        double cosPitch = Transformations.dot(shooterDirection.toTranslation2d(), shooterDirectionHorizontal);
+        double pitch = Math.acos(cosPitch);
+        double cotPitch = cosPitch / Math.sin(pitch);
 
         // calculate robot (center) distance to speaker
         var speakerToShooter = speakerToRobot.plus(robotToShooter);
         double height = Math.abs(speakerToShooter.getZ());
-        double shooterAngle = Math.abs(robotToShooter.getRotation().getX());
-        double firingDistance = height / Math.tan(shooterAngle) - robotToShooter.getY(); // see whiteboard... nevermind, it got erased
+        double shooterFiringDistance = height * cotPitch;
 
         // calculate the actual desired position of the robot
-        double directionRotation = angle + speaker.getRotation().getZ();
-        Translation2d direction = new Translation2d(Math.cos(directionRotation), Math.sin(directionRotation));
-        Translation2d position = speaker.getTranslation().toTranslation2d().plus(direction.times(firingDistance));
+        Rotation2d directionRotation = new Rotation2d(targetAngle).plus(speaker.getRotation().toRotation2d());
+        Translation2d desiredRelativeTranslation = new Translation2d(shooterFiringDistance, directionRotation);
 
-        return new Pose2d(position, new Rotation2d(robotToShooter.getRotation().getZ() - directionRotation));
+        Translation2d desiredPosition = speaker.getTranslation().toTranslation2d().plus(desiredRelativeTranslation);
+        Rotation2d desiredRotation = robotToShooter.getRotation().toRotation2d().minus(directionRotation);
+
+        return new Pose2d(desiredPosition, desiredRotation);
     }
 
     public Command getCommand() {
